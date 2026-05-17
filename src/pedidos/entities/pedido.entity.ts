3a0d352052ccf4c6470e6329b7598a04836be785
jonatasfonsec <jonatas.fonsec@gmail.com>
2026-05-17 19:27:16 -0300

    Criado entidade pedidos e testado integracao com o sqlite

--- src/pedidos/entities/pedido.entity.ts
+++ src/pedidos/entities/pedido.entity.ts
@@ -1,1 +1,23 @@
-export class Pedido {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('pedidos')
+export class Pedido {
+  
+  @PrimaryColumn()
+  codigoPedido: number;
+
+  @Column()
+  nomePaciente: string;
+
+  @Column()
+  dataNascimento: string;
+
+  @Column()
+  sexo: string;
+
+  @Column()
+  codUnidade: number;
+
+  @Column({ default: false })
+  integrado: boolean;
+}
