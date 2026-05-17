2fa1c22cdbc8231834690efb819e9d37d979f9e9
jonatasfonsec <jonatas.fonsec@gmail.com>
2026-05-17 19:31:36 -0300

    Criadas entidades exames e documentos

--- src/documentos/entities/documento.entity.ts
+++ src/documentos/entities/documento.entity.ts
@@ -1,1 +1,23 @@
-export class Documento {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('documentos')
+export class Documento {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  codigoDocumento: number;
+
+  @Column()
+  codigoPedido: number;
+
+  @Column()
+  nomeDocumento: string;
+
+  @Column('text')
+  documento: string;
+
+  @Column({ default: false })
+  integrado: boolean;
+}
--- src/exames/entities/exame.entity.ts
+++ src/exames/entities/exame.entity.ts
@@ -1,1 +1,17 @@
-export class Exame {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('exames')
+export class Exame {
+
+  @PrimaryColumn()
+  accessionNumber: string;
+
+  @Column()
+  nomePaciente: string;
+
+  @Column()
+  modalidade: string;
+
+  @Column()
+  status: string;
+}
