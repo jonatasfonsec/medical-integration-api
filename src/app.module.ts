61f47d5dc10f77f5f8c6351b0132f72491b5f99b
jonatasfonsec <jonatas.fonsec@gmail.com>
2026-05-17 19:06:12 -0300

    Criado estrutura das entidades com o Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { DocumentosModule } from './documentos/documentos.module';
+import { ExamesModule } from './exames/exames.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+
+      database: 'data/database.sqlite',
+
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+
+      synchronize: true,
+    }),
+    PedidosModule,
+    DocumentosModule,
+    ExamesModule,
+  ],
 })
 export class AppModule {}
